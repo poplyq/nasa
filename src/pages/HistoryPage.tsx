2fa04c7b7782a85754acb2044aaec213009b4b0d
Aziz Halilov <poplyq@live.com>
2023-07-06 22:14:08 +0300

    fix bugs once more

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -4,12 +4,11 @@ import { useAppDispatch, useAppSelector } from '../store/store'
 import HistoryModule from '../modules/HistoryModule'
 
 const HistoryPage = () => {
-  const { user } = useAppSelector((state) => state.userState)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    user && dispatch(getHistory())
-  }, [dispatch, user])
+    dispatch(getHistory())
+  }, [dispatch])
   const { history } = useAppSelector((state) => state.historyState)
 
   return <HistoryModule histories={history} />

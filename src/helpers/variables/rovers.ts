35bfc1848447f0497b626e8de47cbd31b169e801
Aziz Halilov <poplyq@live.com>
2023-07-12 23:26:52 +0300

    refactor helpers

--- src/helpers/variables/rovers.ts
+++
@@ -1,7 +0,0 @@
-import { RoversType } from '../../types/ui/rover'
-
-export const rovers: RoversType[] = [
-  { name: 'Curiosity', param: 'curiosity' },
-  { name: 'Opportunity', param: 'opportunity' },
-  { name: 'Spirit', param: 'spirit' },
-]
